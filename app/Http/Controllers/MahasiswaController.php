8e990647a4ec3b00107ba06f0d45b446338fdd11
Desy Ayurianti <desii.ayurianti@gmail.com>
2023-05-17 15:30:44 +0700

    Praktikum 5 : Menghapus data dengan Rest API

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -106,6 +106,7 @@ class MahasiswaController extends Controller
      */
     public function destroy(Mahasiswa $mahasiswa)
     {
-        //
+        $mahasiswa->delete();
+        return response()->noContent();
     }
 }
